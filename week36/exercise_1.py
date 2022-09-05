68190b5d363148e0d5386a9c86aa2cd688e91a45
Cammie <cami834j@gmail.com>
2022-09-06 00:04:16 +0200

    week36 exercises

---
+++ week36/exercise_1.py
@@ -0,0 +1,28 @@
+#EX1
+#ints
+print(2+2)
+print(2*2)
+print(2/2)
+
+#string
+print("Hi" + "Everyone")
+print("yes there can")
+#print("Hi" * "Everyone")
+#TypeError: can't multiply sequence by non-int of type 'str'
+#print("hi" - "everyone")
+#TypeError: unsupported operand type(s) for -: 'str' and 'str'
+
+#Datastructures
+liste = ["this", "is", "difinitely", "five", "strings"]
+print(liste[3])
+print(max(liste))
+hobbydictionary = {
+  "person1": "thing1",
+  "person2": "thing2",
+  "person3": "thing3",
+  "person4": "thing4",
+  "person5": "thing5"
+}
+print(hobbydictionary)
+print(hobbydictionary["person1"])
+
---
+++ week36/exercise_2.py
@@ -0,0 +1,10 @@
+#1. No Max Limit
+#2. Max float
+import sys
+print(sys.float_info.max)
+#1.7976931348623157e+308
+#3. Max length of string is about 2bil. Probably never gonna have one that long.
+#4. Blande datatyper i en liste
+liste = ["this", ['i','s'], "difinitely", 5, "strings"]
+print(liste)
+#5. Strings in a dictionary 
