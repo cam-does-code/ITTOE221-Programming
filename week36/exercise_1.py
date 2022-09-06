bab8d0f415f0711f339eda4c0cf291e549a07e1e
Cammie <cami834j@gmail.com>
2022-09-06 10:46:09 +0200

    update ex2

--- week36/exercise_1.py
+++ week36/exercise_1.py
@@ -1,28 +1,33 @@
-#EX1
-#ints
-print(2+2)
-print(2*2)
-print(2/2)
+# #EX1
+# #ints
+# print(2+2)
+# print(2*2)
+# print(2/2)
 
-#string
-print("Hi" + "Everyone")
-print("yes there can")
-#print("Hi" * "Everyone")
-#TypeError: can't multiply sequence by non-int of type 'str'
-#print("hi" - "everyone")
-#TypeError: unsupported operand type(s) for -: 'str' and 'str'
+# #string
+# #1.
+# print("Hi" + "Everyone")
+# #2.
+# print("yes there can")
+# #3.
+# #print("Hi" * "Everyone")
+# #TypeError: can't multiply sequence by non-int of type 'str'
+# #4.
+# #print("hi" - "everyone")
+# #TypeError: unsupported operand type(s) for -: 'str' and 'str'
 
-#Datastructures
-liste = ["this", "is", "difinitely", "five", "strings"]
-print(liste[3])
-print(max(liste))
-hobbydictionary = {
-  "person1": "thing1",
-  "person2": "thing2",
-  "person3": "thing3",
-  "person4": "thing4",
-  "person5": "thing5"
-}
-print(hobbydictionary)
-print(hobbydictionary["person1"])
+# #Datastructures
+# liste = ["this", "is", "difinitely", "five", "strings"]
+# print(liste[3])
+# print(max(liste))
+while(True):
+  hobbydictionary = {
+    "person1": "thing1",
+    "person2": "thing2",
+    "person3": "thing3",
+    "person4": "thing4",
+    "person5": "thing5"
+  }
+  #print(hobbydictionary)
+  print(hobbydictionary[input("Skriv hvad du vil søge på: \n")])
 
