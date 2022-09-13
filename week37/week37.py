c5aae133f8d30a4865f401f02fb40e2cb451253f
Cammie <cami834j@gmail.com>
2022-09-13 14:42:23 +0200

    ex01

--- week37/week37.py
+++ week37/week37.py
@@ -23,25 +23,46 @@ metascore = list(table['Metascore'])
 antal_vurderinger = list(table['Votes'])
 instruktører = list(table['Director'])
 
-#Prints the first 10 titles
-for i in range(10):
-    print(titel[i])
+# #Prints the first 10 titles
+# for i in range(10):
+#     print(titel[i])
 
-#Prints tiles with rating < 8.0 in the first 80 titles
-for i in range(80):
-    if vurdering[i] < 8.0:
-        print(titel[i])
+# #Prints tiles with rating < 8.0 in the first 80 titles
+# for i in range(80):
+#     if vurdering[i] < 8.0:
+#         print(titel[i])
 
-#Print all titles above a length of 10 chars
-for i in titel:
-    if(len(i) > 10):
-        print(i)
+# #Print all titles above a length of 10 chars
+# for i in titel:
+#     if(len(i) > 10):
+#         print(i)
 
-#Counts number of metascores above 80
-count = 0
-for i in metascore:
-    if(i > 80):
-        count = count+1
-print(count)
+# #Counts number of metascores above 80
+# count = 0
+# for i in metascore:
+#     if(i > 80):
+#         count = count+1
+# print(count)
 
-print(len(titel))
+# #Printer hvor mange titler der er i listen
+# print(len(titel))
+
+# #Printer listen
+# print(titel)
+
+# #Print bestemt titel hvis den er der
+# for i in titel:
+#     if(i == 'The Lion King'):
+#         print(i)
+
+# for i in instruktører:
+#     if(i == 'Wes Anderson'):
+#         print(i)
+
+counter = 0
+for i in range(len(instruktører)):
+    if instruktører[i] == 'Hayao Miyazaki':
+        counter += 1
+print(counter)
+
+print(instruktører.count('Hayao Miyazaki'))
